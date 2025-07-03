a07ef6c423c66c689c579169c8dd191afea9a4d0
justkiduss <kidusteshome036@gmail.com>
2025-07-03 12:38:33 +0300

    adding checkauth function and modifiying it

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8080/api',
-	// baseURL: 'https://movie-site-production-2779.up.railway.app/api',
+	// baseURL: 'http://localhost:8080/api',
+	baseURL: 'https://movie-site-production-2779.up.railway.app/api',
 	withCredentials: true,
 });
 export default axiosInstance;
