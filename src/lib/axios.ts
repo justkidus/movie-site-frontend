cd48b08d296d173e079bfad441054d194a0d748e
justkiduss <kidusteshome036@gmail.com>
2025-07-03 21:37:21 +0300

    adding checkauth function and modifiying it

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	// baseURL: 'http://localhost:8080/api',
-	baseURL: 'https://movie-site-production-2779.up.railway.app/api',
+	baseURL: 'https://trailerflix-9olg.onrender.com/api',
 	withCredentials: true,
 });
 export default axiosInstance;
