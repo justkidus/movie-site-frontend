043f1212fe81efdc4e99bd2723239f31550154e4
justkiduss <kidusteshome036@gmail.com>
2025-07-09 23:24:39 +0300

    updatting axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8080/api',
-	// baseURL: 'https://trailerflix-9olg.onrender.com/api',
+	// baseURL: 'http://localhost:8080/api',
+	baseURL: 'https://trailerflix-9olg.onrender.com/api',
 	withCredentials: true,
 });
 export default axiosInstance;
